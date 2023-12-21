51837a299d89891c63496e291ebe4291a1be604e
thatvisualguy <124064502+thatvisualguy@users.noreply.github.com>
2023-12-21 15:10:32 +0530

    Update scripts_home to V2.js

--- scripts_home.js
+++ scripts_home.js
@@ -33,3 +33,20 @@ function myAccFunc1() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+//Mockup Crousel
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "inline";
+  x[myIndex-1].style.animation = "slide-left 1s forwards";
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
